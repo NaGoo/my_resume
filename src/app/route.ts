08e7552a7acf3c3bc74f971fb64a6cae79704f1f
NaGoo <nagarajan.subramanian@readyassist.in>
2022-04-28 11:34:35 +0530

    UI updated

--- src/app/route.ts
+++ src/app/route.ts
@@ -1,13 +1,13 @@
 import {Routes} from '@angular/router';
 
 export const routes: Routes = [
+  // {
+  //   path: '',
+  //   pathMatch: 'full',
+  //   redirectTo: '/home'
+  // },
   {
     path: '',
-    pathMatch: 'full',
-    redirectTo: '/home'
-  },
-  {
-    path: 'home',
     loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule)
   },
 ]
